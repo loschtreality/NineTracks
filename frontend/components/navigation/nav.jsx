a3111527046961f42733b73c630d19e5d711ea69
Michael Loschiavo <losch103@gmail.com>
2016-11-02 10:51:29 -0700

    Add bootstrap to rails. Add Navigation component

--- frontend/components/navigation/nav.jsx
+++ frontend/components/navigation/nav.jsx
@@ -4,7 +4,63 @@ import SessionFormContainer from '../session_form/session_form_container';
 
 
 class Nav extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse">
+  <div className="container-fluid">
 
+    <div className="navbar-header">
+      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+      <a className="navbar-brand" href="#">NineTracks</a>
+    </div>
+
+
+    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul className="nav navbar-nav">
+        <li className="active"><Link href="#">Home <span className="sr-only">(current)</span></Link></li>
+        <li><a href="#">Link</a></li>
+        <li className="dropdown">
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+          <ul className="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" className="divider"></li>
+            <li><a href="#">Separated link</a></li>
+            <li role="separator" className="divider"></li>
+            <li><a href="#">One more separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form className="navbar-form navbar-left">
+        <div className="form-group">
+          <input type="text" className="form-control" placeholder="Search"></input>
+        </div>
+        <button type="submit" className="btn btn-default">Submit</button>
+      </form>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#">Link</a></li>
+        <li className="dropdown">
+          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Thing <span className="caret"></span></a>
+          <ul className="dropdown-menu">
+            <li><a href="#">Action</a></li>
+            <li><a href="#">Another action</a></li>
+            <li><a href="#">Something else here</a></li>
+            <li role="separator" className="divider"></li>
+            <li><a href="#">Separated link</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+    );
+  }
 }
 
 export default Nav;
